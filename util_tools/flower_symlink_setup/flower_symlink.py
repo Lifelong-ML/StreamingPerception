d687e842c160bbbf681b774f073c54692ee300ee
Spencer Solit <ssolit@kostas-ap.seas.upenn.edu>
2021-06-11 11:50:32 -0400

    syncing util_tools

---
+++ util_tools/flower_symlink_setup/flower_symlink.py
@@ -0,0 +1,3 @@
+import scipy.io
+mat = scipy.io.loadmat('/mnt/Data/Streaming_Data/flowers/imagelabels.mat')
+print(mat)
--- util_tools/image_test.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import numpy as np
-
-image = Image.open("/Datasets/CUB_200_2011/images/001.Black_footed_Albatross/Black_Footed_Albatross_0090_796077.jpg")
-width, height = image.size
-print(width, height)
-print(np.array(image).shape)
